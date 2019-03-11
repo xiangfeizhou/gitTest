0a4475b5744369f49bff234cad9e2ae5eb84fa36
xiangfeizhou <46594773+xiangfeizhou@users.noreply.github.com>
2019-03-12 00:10:32 +0800

    看看这次能不能成功

---
+++ src/main/java/com/example/xiangfeizhou/XiangfeizhouApplication.java
@@ -0,0 +1,13 @@
+package com.example.xiangfeizhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangfeizhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiangfeizhouApplication.class, args);
+    }
+
+}
